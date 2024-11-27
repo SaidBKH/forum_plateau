eddf131ea7df93a00cbccb83a12049c997fc82f5
SaidBKH <boukhdimisaid@gmail.com>
2024-11-27 10:37:12 +0100

    rework du forum

--- view/home.php
+++ view/home.php
@@ -27,94 +27,109 @@ $statTopics = $result["data"]["statTopics"];
 
     <?php } else if(Session::getUser()) { ?>
 
-        <h1>BIENVENUE SUR LE FORUM <?= $_SESSION["user"]->getNickName() ?></h1>
+        <h1>BIENVENUE <?= $_SESSION["user"]->getNickName() ?></h1>
 
     <?php } else { ?>
         
-        <h1>BIENVENUE SUR LE FORUM</h1>
+        <h1>BIENVENUE SUR IA ODYSSEE</h1>
         <br>
-            <p>Vous devez vous inscrire ou vous connecter au forum pour accéder
-            au contenu du forum et à tout nos services.
+            <p>Bienvenue dans l’univers fascinant de l’intelligence artificielle ! 
+                <br>
+                <br>
+                <br>
+             IA Odyssée est un forum dédié à tous ceux qui souhaitent discuter des dernières avancées, 
+            partager des connaissances et découvrir les innovations du domaine de l'IA. 
+            Rejoignez-nous pour accéder à nos discussions et ressources exclusives.
             </p>
-</div>
 
-<div id="loginRegister">
-    <div id="loginForm">
-         <h2> login</h2>
+
+
+    <div id="loginRegister">
+        <div id="loginForm">
+            <h2>Login</h2>
             <form action="index.php?ctrl=security&action=login" method="post">
                 <label for="email"></label><br>
                 <input placeholder="mail" type="email" id="email" name="email" required style="text-align: center;"><br>
                 <label for="password"></label><br>    
-                <input placeholder=" mot de passe" type="password" id="password" name="password" required style="text-align: center;"><br>
+                <input placeholder="mot de passe" type="password" id="password" name="password" required style="text-align: center;"><br>
                 <button type="submit">SE CONNECTER</button>
             </form>
-    </div>
-    <br>
+        </div>
+        <br>
     
         <div class="registerButton">
-            <button name="register"> <a href="index.php?ctrl=security&action=registerForm">JE M'INSCRIS </button>
+            <button name="register"> <a href="index.php?ctrl=security&action=registerForm">JE M'INSCRIS</a></button>
         </div>
-        <?php } ?>
     </div>
+    <?php } ?>
+</div>
 
 <div class = "homeList">
     <div class="listCategory">
         <h1>LISTE DES CATEGORIES</h1>
         <br>
-            <ul>
-                <?php foreach($categories as $category ){ ?>
-                <li><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><span style="text-transform:uppercase"><?= $category->getName() ?></span></a></li>
-                <?php }?>
-            </ul>
+        <ul>
+            <?php foreach($categories as $category): ?>
+                <li>
+                    <a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>">
+                        <span style="text-transform:uppercase"><?= $category->getName() ?></span>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 
     <div class="lastTopic">
         <br>
-            <h1>DERNIERS SUJETS</h1>
+        <h1>DERNIERS SUJETS</h1>
         <br>
-            <ul>
+        <ul>
             <?php foreach ($latestTopics as $topic): ?>
-                 <li>
-                        <p>category :<a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $topic->getCategory()->getId() ?>" ><?= $topic->getCategory()?></a><br>
-                        <a href="index.php?ctrl=forum&action=listPostsByTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?></a>  
-                         par :  <a href="index.php?ctrl=forum&action=detailProfil&id=<?= $topic->getUser()->getId() ?>" ><?= $topic->getUser() ?> </a>
-                        publié le : <?= $topic->getCreationDate() ?></p>     
+                <li>
+                    <p>Catégorie : <a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $topic->getCategory()->getId() ?>">
+                        <?= $topic->getCategory() ?></a><br>
+                        <a href="index.php?ctrl=forum&action=listPostsByTopic&id=<?= $topic->getId() ?>">
+                            <?= $topic->getTitle() ?>
+                        </a>  
+                        par : <a href="index.php?ctrl=forum&action=detailProfil&id=<?= $topic->getUser()->getId() ?>">
+                            <?= $topic->getUser() ?>
+                        </a>
+                        publié le : <?= $topic->getCreationDate() ?>
+                    </p>
                 </li>
-                <?php endforeach; ?>
-            </ul>    
+            <?php endforeach; ?>
+        </ul>    
     </div>
 </div>
 
 <div class="statistics">
     <h1>STATISTIQUES</h1>
-        <table>                
-            <tr>
-                <td><a href="index.php?ctrl=forum&action=listUser">Membres :</a></td>
-                <td>
-                    <?php foreach($statUsers as $stat) { 
-                    echo  $stat->getReCount()." membres";                                                                 
-                    } ?>
-                </td>
-            </tr>
+    <table>                
+        <tr>
+            <td><a href="index.php?ctrl=forum&action=listUser">Membres :</a></td>
+            <td>
+                <?php foreach($statUsers as $stat): ?>
+                    <?= $stat->getReCount() . " membres" ?>
+                <?php endforeach; ?>
+            </td>
+        </tr>
 
-            <tr>
-                <td>Topics :</td>
-                <td>
-                     <?php foreach($statTopics as $stat) { 
-                     echo $stat->getReCount() ." discussions";
-                    } ?>
-                </td>
-            </tr>
+        <tr>
+            <td>Topics :</td>
+            <td>
+                <?php foreach($statTopics as $stat): ?>
+                    <?= $stat->getReCount() . " discussions" ?>
+                <?php endforeach; ?>
+            </td>
+        </tr>
 
-            <tr>
-                <td>Posts :</td>    
-                <td>
-                    <?php foreach($statPosts as $stat) {
-                    echo $stat->getReCount()." messages";
-                    } ?>
-                </td>
-            </tr>
+        <tr>
+            <td>Posts :</td>    
+            <td>
+                <?php foreach($statPosts as $stat): ?>
+                    <?= $stat->getReCount() . " messages" ?>
+                <?php endforeach; ?>
+            </td>
+        </tr>
     </table>            
 </div>
-             
-
--- view/layout.php
+++ view/layout.php
@@ -1,90 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="<?= $meta_description ?>">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-        <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
-        <title>FORUM</title>
-    </head>
-    <body>
-        <div id="wrapper"> 
-            <div id="mainpage">
-                <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
-                <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
-                <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
-                <header>
-                        <nav>
-                            <div id="nav-left">
-                                <a href="index.php">ACCUEIL</a>
-                               <!-- <a href="index.php?ctrl=forum&action=listCategory">CATEGORIE</a> -->
-                            </div>
-
-                            <div id="nav-right">
-                                <?php if(App\Session::getUser()): ?>
-                                    <a href="index.php?ctrl=user&action=profile">
-                                    <span class="fas fa-user"></span>
-                                    &nbsp;<?= App\Session::getUser()->getNickName() ?>
-                                    </a>
-                                    <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
-                                <?php else: ?>
-                                    <a href="index.php?ctrl=security&action=loginForm">SE CONNECTER </a>
-                                    <a href="index.php?ctrl=security&action=registerForm">S'INSCRIRE</a>
-                                <?php endif; ?>
-
-                            </div>
-                        </nav>
-                        
-                </header>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?= $meta_description ?>">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <title>AutoTalk - L</title>
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <nav>
+                <div id="nav-left">
+                   
+                    <a href="index.php">ACCUEIL</a>
+                    <a href="index.php?ctrl=forum&action=listCategory">CATÉGORIES</a>
+                    <a href="index.php?ctrl=forum&action=latestNews">ACTUALITÉS</a>
+                </div>
+                <div class = "logo">
+                <a href="index.php">
+                        <img src="<?= PUBLIC_DIR ?>/img/logo.png" alt="Logo du forum" id="forum-logo" >
+                    </a>
+                </div>
                 
-                <main id="forum">
-                    <?= $page ?>
-                </main>
-            </div>
-            <footer>
-            <div class ="footer">
-                <br>
+                <div id="nav-right">
+                    <?php if(App\Session::getUser()): ?>
+                        <a href="index.php?ctrl=user&action=profile">
+                            <span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()->getNickName() ?>
+                        </a>
+                        <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
+                    <?php else: ?>
+                        <a href="index.php?ctrl=security&action=loginForm">SE CONNECTER</a>
+                        <a href="index.php?ctrl=security&action=registerForm">S'INSCRIRE</a>
+                    <?php endif; ?>
+                </div>
+            </nav>
+        </header>
+
+        <main id="forum">
+            <?= $page ?>
+        </main>
+
+        <footer>
+            <div class="footer">
                 <p>&copy; <?= date_create("now")->format("Y") ?> - <a href="#">Règlement du forum</a> - <a href="#">Mentions légales</a></p>
-            </footer>
-        </div>
-        <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous">
-        </script>
-        <script>
-            $(document).ready(function(){
-                $(".message").each(function(){
-                    if($(this).text().length > 0){
-                        $(this).slideDown(500, function(){
-                            $(this).delay(3000).slideUp(500)
-                        })
-                    }
-                })
-                $(".delete-btn").on("click", function(){
-                    return confirm("Etes-vous sûr de vouloir supprimer?")
-                })
-                tinymce.init({
-                    selector: '.post',
-                    menubar: false,
-                    plugins: [
-                        'advlist autolink lists link image charmap print preview anchor',
-                        'searchreplace visualblocks code fullscreen',
-                        'insertdatetime media table paste code help wordcount'
-                    ],
-                    toolbar: 'undo redo | formatselect | ' +
-                    'bold italic backcolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                    content_css: '//www.tiny.cloud/css/codepen.min.css'
-                });
-            })
-        </script>
-        <script src="<?= PUBLIC_DIR ?>/js/script.js"></script>
-    </body>
+            </div>
+        </footer>
+    </div>
 
-
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $(".message").each(function(){
+                if($(this).text().length > 0){
+                    $(this).slideDown(500, function(){
+                        $(this).delay(3000).slideUp(500)
+                    });
+                }
+            });
+            $(".delete-btn").on("click", function(){
+                return confirm("Êtes-vous sûr de vouloir supprimer?");
+            });
+            tinymce.init({
+                selector: '.post',
+                menubar: false,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+                content_css: '//www.tiny.cloud/css/codepen.min.css'
+            });
+        });
+    </script>
+    <script src="<?= PUBLIC_DIR ?>/js/script.js"></script>
+</body>
 </html>
